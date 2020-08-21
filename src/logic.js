b85124679e89467e86423cd070a5c8673c765e19
Rob9315 <rob-kon@gmx.de>
2020-08-21 22:20:28 +0200

    fix

--- src/logic.js
+++ src/logic.js
@@ -1,3 +1,5 @@
+module.exports = { ini, input, login };
+
 //* import
 const config = require('../config.json');
 const Chunk = require('prismarine-chunk')(config.version);
@@ -17,33 +19,33 @@ function ini(c) {
 function input(packet) {
 	relay(packet);
 	var { data, meta } = packet;
-
-	if (config.specialPackets.includes(meta.name)) {
-		switch (meta.name) {
-			case 'login':
+	switch (meta.name) {
+		case 'login':
+			saveData(data);
+			setDimension();
+			break;
+		case 'map_chunk':
+			saveChunk(data);
+			break;
+		case 'unload_chunk':
+			unloadChunk(data);
+			break;
+		case 'block_change':
+			blockChange(data);
+			break;
+		case 'game_state_change':
+			saveData(packet, 'reason');
+			break;
+		case 'respawn':
+			saveData(packet);
+			setDimension();
+			break;
+		case 'update_time':
+			savePacket();
+		default:
+			if (!config.bad_packets.includes(meta.name)) {
 				saveData(data);
-				setDimension();
-				break;
-			case 'map_chunk':
-				saveChunk(data);
-				break;
-			case 'unload_chunk':
-				unloadChunk(data);
-				break;
-			case 'block_change':
-				blockChange(data);
-				break;
-			case 'game_state_change':
-				saveData(packet, 'reason');
-				break;
-			case 'respawn':
-				saveData(packet);
-				setDimension();
-				break;
-			default:
-		}
-	} else {
-		saveData(data);
+			}
 	}
 	break;
 }
@@ -95,12 +97,12 @@ function send({ data, meta }, sender) {
 }
 
 function statusReport(newProxyClient) {
-	buildSendIt(newProxyClient)
-	sendChunks(newProxyClient)
+	buildSendIt(newProxyClient);
+	sendChunks(newProxyClient);
 }
 
-function sendChunks(newProxyClient){
-	columnArray = world.getColumns()
+function sendChunks(newProxyClient) {
+	columnArray = world.getColumns();
 	for (const x in columnArray) {
 		if (columnArray.hasOwnProperty(x)) {
 			const arr = columnArray[x];
@@ -117,16 +119,51 @@ function sendChunks(newProxyClient){
 }
 
 function buildSendIt(nPC) {
-	send({data = {
-		difficulty: client.difficulty,
-		dimension: client.dimension,
-		entityId: client.entityId,
-		gameMode: client.gameMode,
-		levelType: client.levelType,
-		maxPlayers: client.maxPlayers,
-		reducedDebugInfo: client.reducedDebugInfo
-	}, meta = {name = 'login'}}, nPC)
-	send()
+	send(
+		{
+			data: {
+				difficulty: client.difficulty,
+				dimension: client.dimension,
+				entityId: client.entityId,
+				gameMode: client.gameMode,
+				levelType: client.levelType,
+				maxPlayers: client.maxPlayers,
+				reducedDebugInfo: client.reducedDebugInfo,
+			},
+			meta: { name: 'login' },
+		},
+		nPC
+	);
+	send(
+		{
+			data: {
+				flags: client.flags,
+				flyingSpeed: client.flyingSpeed,
+				walkingSpeed: client.walkingSpeed,
+			},
+			meta: { name: 'abilities' },
+		},
+		nPC
+	);
+	send({ data: { slot: client.slot }, meta: { name: 'held_item_slot' } }, nPC);
+	send(
+		{
+			data: {
+				flags: client.flags,
+				pitch: client.pitch,
+				teleportId: client.teleportId,
+				x: client.x,
+				y: client.y,
+				yaw: client.yaw,
+				z: client.z,
+			},
+			meta: { name: 'position' },
+		},
+		nPC
+	);
+	send({ data: client.worldBorder, meta: { name: 'world_border' } }, nPC);
+	send({ data: client.updateTime, meta: { name: 'update_time' } }, nPC);
+	send({ data: client.advancements, meta: { name: 'advancements' } }, nPC);
 }
 
 function buildChunkPacket({ x, z, chunk }) {
@@ -140,4 +177,17 @@ function buildChunkPacket({ x, z, chunk }) {
 		blockEntities: [],
 	};
 	return { data, meta };
+}
+
+function savePacket({ data, meta }) {
+	var toadd = true;
+	client.packets.forEach((packet, index) => {
+		if (packet.meta.name == meta.name) {
+			client.packets[index] = { data, meta };
+			toadd = false;
+		}
+	});
+	if (toadd) {
+		client.packets.push({ data, meta });
+	}
 }
